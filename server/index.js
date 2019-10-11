9ff2ebceafafb952e1b7c6cdef916985e7824621
slarsendisney <s.larsendisney@gmail.com>
2019-10-11 23:36:57 +0100

    poll

--- server/index.js
+++ server/index.js
@@ -3,20 +3,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const compression = require("compression");
-const io = require("socket.io")(server);
 
 const dev = !(process.env.NODE_ENV === "production");
 
 app.use(compression());
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
-
-if (!dev) {
-  io.configure(function() {
-    io.set("transports", ["xhr-polling"]);
-    io.set("polling duration", 10);
-  });
-}
 
 if (dev) {
   require("dotenv").config();
@@ -36,6 +28,15 @@ const port = process.env.PORT || 3000;
 server.listen(port, () =>
   console.log(`🍉 Up and running on http://localhost:${port}`)
 );
+
+const io = require("socket.io")(server);
+
+if (!dev) {
+  io.configure(function() {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+  });
+}
 
 io.on("connection", function(socket) {
   console.log("socket connected: " + socket.id);
