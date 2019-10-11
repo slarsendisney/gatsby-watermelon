1634a660136f11e92f17b0fc58930c20e8dedc2d
slarsendisney <s.larsendisney@gmail.com>
2019-10-11 12:33:18 +0100

    removed deploy script

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Logo from "../images/watermelon.png";
 
 export default () => (
   <div className="is-white-bg margin-5">
     <div className="row flex padding-0-tb container-small">
       <div className="col-xs-9 flex">
-        <h1>🍉</h1>
+        <img src={Logo} className="logo" />
       </div>
     </div>
   </div>
